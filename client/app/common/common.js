68be5cd37ef28f6a2a708d23bd5e094ae9da7d1b
Guillaume Coste <guillaume.coste@livingobjects.com>
2015-12-11 14:31:24 +0100

    register toolbar

--- client/app/common/common.js
+++ client/app/common/common.js
@@ -1,10 +1,12 @@
 import angular from 'angular';
 import User from './user/user';
 import Sidemenu from './sidemenu/sidemenu';
+import Toolbar from './toolbar/toolbar';
 
 let commonModule = angular.module('app.common', [
+    Sidemenu.name,
     User.name,
-    Sidemenu.name
+    Toolbar.name
 ]);
 
 export default commonModule;
